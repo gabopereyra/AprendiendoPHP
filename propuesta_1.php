c1cb14cc79ebc7f9255ca186fac257039be8185e
gabopereyra <gabrielp1689qgmail.com>
2020-11-23 15:42:51 -0300

    Primer propuesta.

---
+++ propuesta_1.php
@@ -0,0 +1,16 @@
+<html>
+
+<head></head>
+
+<body>
+  <?php
+  echo "Esta es la primer propuesta del tutorial";
+  echo "<br>";
+  echo "Desarrollar varias lineas de texto, a través del comando 'echo'";
+  echo "<br>";
+  echo "<br>";
+  echo "Tutorial by 'Tutoriales Ya'";
+  ?>
+</body>
+
+</html>
