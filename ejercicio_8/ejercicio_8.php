94af0ac920de847a165ad32ef9a91c265f16c21b
gabopereyra <gabrielp1689qgmail.com>
2020-11-28 10:37:37 -0300

    Chechbox, utilizacion y procesamiento

---
+++ ejercicio_8/ejercicio_8.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <form action="ejercicio_8_post.php" method="post">
+    Ingrese primer valor:
+    <input type="text" name="valor1">
+    <br>
+    Ingrese segundo valor:
+    <input type="text" name="valor2">
+    <br>
+    <input type="checkbox" name="check1">sumar
+    <br>
+    <input type="checkbox" name="check2">restar
+    <br>
+    <input type="submit" name="operar">
+  </form>
+</body>
+
+</html>
