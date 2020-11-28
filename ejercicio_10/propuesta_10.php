71d842281b1f3920f2922f115606fb8f89e3acb1
gabopereyra <gabrielp1689qgmail.com>
2020-11-28 20:07:47 -0300

    Ejercicio de aplicacion del TEXTAREA

---
+++ ejercicio_10/propuesta_10.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <form action="propuesta_10_post.php" method="post">
+      <p>Confeccionar una página que muestre un contrato dentro de un textarea, disponer puntos suspensivos donde el operador debe ingresar un texto.</p>
+      <p>La página que procesa el formulario sólo debe mostrar el contrato con las modificaciones que hizo el operador.</p>
+<hr>
+    <p>Complete el contracto que figura a continuación con los datos que corresponden</p>
+    <br>
+    <textarea name="contrato" rows="10" cols="60">
+        En la ciudad de [........], se acuerda entre la Empresa [..........] 
+        representada por el Sr. [..............] en su carácter de Apoderado,
+        con domicilio en la calle [..............] y el Sr. [..............],
+        futuro empleado con domicilio en [..............], celebrar el presente 
+        contrato a Plazo Fijo, de acuerdo a la normativa vigente de los
+        artículos 90,92,93,94, 95 y concordantes de la Ley de Contrato de Trabajo N° 20.744.</textarea>
+    <br>
+    <input type="submit" value="Confirmar y enviar">
+  </form>
+</body>
+
+</html>
---
+++ ejercicio_10/propuesta_10_post.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+<?php   
+    echo "El contrato es el siguiente:";
+    echo "<br><br>";
+    echo $_REQUEST["contrato"];
+?>
+</body>
+
+</html>
