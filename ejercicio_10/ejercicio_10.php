8e248fe93aa5f1bea09a34a280e25e97cb7f21a3
gabopereyra <gabrielp1689qgmail.com>
2020-11-28 20:00:17 -0300

    TEXTAREA presentacion y recuperacion de dato a traves de PHP

---
+++ ejercicio_10/ejercicio_10.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <form action="ejercicio_10_post.php" method="post">
+    Ingrese nombre:<input type="text" name="nombre"><br>
+    Ingrese su curriculum:<br>
+    <textarea name="curriculum"></textarea>
+    <br>
+    <input type="submit" value="Confirmar">
+  </form>
+</body>
+
+</html>
---
+++ ejercicio_10/ejercicio_10_post.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <?php
+  echo "El nombre ingresado:" . $_REQUEST['nombre'];
+  echo "<br>";
+  echo "El curriculum:" . $_REQUEST['curriculum'];
+  ?>
+</body>
+
+</html>
