f5c8c31cf0723cd7ed14fdc939bcbff60ddc9890
gabopereyra <gabrielp1689qgmail.com>
2020-11-29 19:14:06 -0300

    Propuesta de generacion de un array y lectura del mismo

---
+++ propuesta_11.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+<p>Definir un vector con los nombres de los días de la semana. Luego imprimir el primero y el último elemento del vector.</p>
+
+  <?php
+$dias=array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
+echo "El primer dia de la semana es: ".$dias[0];
+echo "<br>";
+echo "El último dia de la semana es: ".$dias[6];
+
+  ?>
+</body>
+
+</html>
