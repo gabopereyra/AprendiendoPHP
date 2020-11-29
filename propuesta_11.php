31bed7436f348e05894269b5328196200ce60dea
gabopereyra <gabrielp1689qgmail.com>
2020-11-29 19:14:30 -0300

    Agregado de foreach a ejercicio

--- propuesta_11.php
+++ propuesta_11.php
@@ -9,10 +9,12 @@
 
   <?php
 $dias=array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
-echo "El primer dia de la semana es: ".$dias[0];
-echo "<br>";
-echo "El último dia de la semana es: ".$dias[6];
-
+echo "El primer dia de la semana es: ".$dias[0]."<br>"."El último dia de la semana es: ".$dias[6]."<hr>";
+echo "El siguiente es el resultado de haber leido el array con un foreach<br><br>";
+foreach ($dias as $dia){
+    echo $dia;
+    echo "<br>";
+}
   ?>
 </body>
 
