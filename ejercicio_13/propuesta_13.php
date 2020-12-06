579f0799cba9dbd6db3ee78a9f378b1951f33016
gabopereyra <gabrielp1689qgmail.com>
2020-12-06 01:16:38 -0300

    Ejercicio de apertura de archivo (generado en propuesta 12)

---
+++ ejercicio_13/propuesta_13.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <?php
+  $ar = fopen("../ejercicio_12/pedidos.txt", "r") or
+    die("No se pudo abrir el archivo");
+  while (!feof($ar)) {
+    $linea = fgets($ar);
+    $lineasalto = nl2br($linea);
+    echo $lineasalto;
+  }
+  fclose($ar);
+  ?>
+</body>
+
+</html>
