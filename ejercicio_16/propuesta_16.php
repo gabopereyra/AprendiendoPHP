03aa3f57a2d11b8a021738feca2428e9b12efb2e
gabopereyra <gabrielp1689qgmail.com>
2020-12-07 12:45:45 -0300

    Propuesta de carga de datos (SQL)

---
+++ ejercicio_16/propuesta_16.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+    <title>Problema</title>
+</head>
+
+<body>
+    <h1>ALTA DE CURSOS</h1>
+    <p>Implementar las dos páginas necesarias para efectuar el alta de cursos.</p> 
+    <p>Un formulario para ingresar el nombre del curso y otra página donde se efectuará el insert.</p>
+    <hr>
+    <head>
+  <title>Problema</title>
+</head>
+  <form action="propuesta_16_post.php" method="post">
+    Seleccione el curso de su interés: 
+    <select name="codigocurso">
+            <option value="PHP">PHP</option>
+            <option value="ASP">ASP</option>
+            <option value="JSP">JSP</option>
+    </select>
+    <br><br>
+    <input type="submit" value="Confirmar">
+  </form>
+
+</body>
+
+</html>
