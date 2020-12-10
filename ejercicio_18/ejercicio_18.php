11c8654b4d499dd110848e2156a4ec185fba1cb8
gabopereyra <gabrielp1689qgmail.com>
2020-12-10 20:26:29 -0300

    Seleccion de un registro particular (utilizacion WHERE)

---
+++ ejercicio_18/ejercicio_18.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <form action="ejercicio_18_post.php" method="post">
+    Ingrese el mail del alumno a consultar:
+    <input type="text" name="mail">
+    <br><br>
+    <input type="submit" value="BUSCAR">
+  </form>
+</body>
+
+</html>
