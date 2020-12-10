eb702c0d9494d28c3220072090bb755718542080
gabopereyra <gabrielp1689qgmail.com>
2020-12-10 20:39:01 -0300

    Propuesta de lectura de registros según input de nombre

---
+++ ejercicio_18/propuesta_18.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+<p>Confeccionar un programa que permita ingresar el nombre de un alumno en un formulario, luego mostrar los datos del mismo (tener en cuenta que puede haber más de un alumno con el mismo nombre)</p>
+<hr>
+  <form action="propuesta_18_post.php" method="post">
+    Ingrese el nombre del alumno a consultar:
+    <input type="text" name="nombre">
+    <br><br>
+    <input type="submit" value="BUSCAR">
+  </form>
+</body>
+
+</html>
