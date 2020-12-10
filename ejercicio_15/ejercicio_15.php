20aedb6d9293afbd9080136bf6037538739f2702
gabopereyra <gabrielp1689qgmail.com>
2020-12-10 20:37:17 -0300

    Se mueve de ubicación

---
+++ ejercicio_15/ejercicio_15.php
@@ -0,0 +1,57 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+<h1 style="text-align:center; color: blue" >FUNCIONES</h1>
+<hr>
+<p>Primer ejemplo</p>
+<p>Funcion para mostrar un titulo (h3) centrado</p>
+  <?php
+  function mostrartitulo($men)
+  {
+    echo "<h3 style=\"text-align:center\">";
+    echo $men;
+    echo "</h3>";
+  }
+
+  mostrartitulo("Primer titulo");
+  echo "<br>";
+  mostrartitulo("Segundo segundo");
+  ?>
+
+<hr>
+<p>Segundo ejemplo</p>
+<p>Funcion para retornarpromedio un promedio entre dos valores</p>
+<?php
+  function retornarpromedio($valor1, $valor2)
+  {
+    $pro = $valor1 / $valor2;
+    return $pro;
+  }
+
+  $v1 = 100;
+  $v2 = 50;
+  $p = retornarpromedio($v1, $v2);
+  echo $p;
+  ?>
+<hr>
+<p>Tercer ejemplo</p>
+<p>Funcion con empleo de parametros por referencia</p>
+
+<?php
+  function cuadradocubo($valor, &$cuad, &$cub)
+  {
+    $cuad = $valor * $valor;
+    $cub = $valor * $valor * $valor;
+  }
+  cuadradocubo(2, $c1, $c2);
+  echo "El cuadrado de 2 es: " . $c1 . "<br>";
+  echo "El cubo de 2 es: " . $c2;
+  ?>
+
+</body>
+
+</html>
