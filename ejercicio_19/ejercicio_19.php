956c4546d7809ab41b3b603a0b3e1f039976f2b2
gabopereyra <gabrielp1689qgmail.com>
2020-12-12 10:31:14 -0300

    Ejercicio de borrado de registro seleccionado

---
+++ ejercicio_19/ejercicio_19.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <form action="ejercicio_19_post.php" method="post">
+    Ingrese el mail del alumno a borrar:
+    <input type="text" name="mail">
+    <br>
+    <input type="submit" value="buscar y borrar">
+  </form>
+</body>
+
+</html>
