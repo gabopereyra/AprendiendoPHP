df4a5f1ed7627f681d7b59e463b22faa2380f6a6
gabopereyra <gabrielp1689qgmail.com>
2020-12-30 20:38:23 -0300

    Propuesta de INNER JOIN, busqueda de un dato segun un valor pasado en un form.

---
+++ ejercicio_22/propuesta_22.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <form action="propuesta_22_post.php" method="post">
+    Ingrese código de alumno:
+    <input type="text" name="codigo"><br><br>
+    <input type="submit" value="Buscar">
+  </form>
+</body>
+
+</html>
