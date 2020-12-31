637313d15da0c69c8f703a29c1415e1e6c811fe3
gabopereyra <gabrielp1689qgmail.com>
2020-12-31 18:08:38 -0300

    Ejercicio de UPDATE utilizando la información de 2 tablas

---
+++ ejercicio_24/ejercicio_24.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <form action="ejercicio_24_post.php" method="post">
+    Ingrese el mail del alumno:
+    <input type="text" name="mail"><br><br>
+
+    <input type="submit" value="BUSCAR">
+  </form>
+</body>
+
+</html>
