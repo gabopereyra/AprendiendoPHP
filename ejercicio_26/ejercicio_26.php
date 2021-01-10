0ee54cffe1135cb3a2924bfa08c05c739ff77b7a
gabopereyra <gabrielp1689qgmail.com>
2021-01-10 11:56:43 -0300

    ejercicio con utilizacion de hipervinculos

---
+++ ejercicio_26/ejercicio_26.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <A href="ejercicio_26_tabla.php?tabla=2">Tabla del 2</A> <br>
+  <A href="ejercicio_26_tabla.php?tabla=3">Tabla del 3</A> <br>
+  <A href="ejercicio_26_tabla.php?tabla=5">Tabla del 5</A>
+</body>
+
+</html>
