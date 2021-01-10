d80db7f77347c9d912bb4264438f2364cc38af4d
gabopereyra <gabrielp1689qgmail.com>
2021-01-10 16:42:41 -0300

    Ejercicio para subir archivo a servidor

---
+++ ejercicio_28/ejercicio_28.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <form action="ejercicio_28_post.php" method="post" enctype="multipart/form-data">
+    Seleccione el archivo:
+    <input type="file" name="archivo">
+    <br>
+    <input type="submit" value="Enviar">
+  </form>
+</body>
+
+</html>
