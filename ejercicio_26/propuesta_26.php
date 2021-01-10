728dc23285667e3f04d63a7157492bdcbca825df
gabopereyra <gabrielp1689qgmail.com>
2021-01-10 12:17:38 -0300

    Propuesta de utilizacion de hipervinculo PHP

---
+++ ejercicio_26/propuesta_26.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Problema</title>
+</head>
+
+<body>
+    <p>Confeccionar un programa que muestre una página con todos los nombres de la tabla "cursos".</p>
+    <p>Los nombres deben aparecer como hipervínculos a otra página que deberá mostrar todos los alumnos inscriptos a dicho curso.</p>
+    <p>Como parámetro en el hipervínculo, pasar el código de curso.</p>
+    <hr>
+    <a href="propuesta_26_result.php?curso=1">HACK</a> <br>
+    <a href="propuesta_26_result.php?curso=2">PHP</a> <br>
+    <a href="propuesta_26_result.php?curso=4">C++</a><br>
+    <a href="propuesta_26_result.php?curso=5">Python</a> <br>
+    <a href="propuesta_26_result.php?curso=6">SQL</a> <br>
+</body>
+
+</html>
---
+++ ejercicio_26/propuesta_26_result.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>Problema</title>
+</head>
+
+<body>
+    <?php
+    $conexion = mysqli_connect("localhost", "root", "", "base1") or
+        die("Problemas con la conexión");
+
+    $registros = mysqli_query($conexion, "select * from alumnos WHERE codigoCurso=$_REQUEST[curso]") or
+        die("Problemas en el select:" . mysqli_error($conexion));
+
+        while ($reg = mysqli_fetch_array($registros)) {
+            echo "<hr>";
+            echo "Alumno:" . $reg['nombre'] . "<br>";
+        }
+    mysqli_close($conexion);
+    ?>
+</body>
+
+</html>
